bed4b65fcfa95d36f191680b50646c6faf2c8944
tofufi <55358373+tofufi@users.noreply.github.com>
2020-11-11 16:23:55 -0500

    Update config.py

--- src/plugin.video.icdrama/lib/config.py
+++ src/plugin.video.icdrama/lib/config.py
@@ -3,7 +3,7 @@ from urlparse import urljoin
 from lib.common import diritem, action_url, profile_dir
 
 base_url = 'http://icdrama.to'
-domains = ['adrama.to', 'icdrama.to', 'icdrama.se']
+domains = ['adrama.to', 'icdrama.to', 'icdrama.se','hkdrama.to']
 cache_file = os.path.join(profile_dir, 'cache.pickle')
 store_file = os.path.join(profile_dir, 'store.pickle')
 
